6e8831a26e0d88d4045f9e323824aff87bb37bc3
Haruka-W <nekonoko1995@gmail.com>
2020-03-04 16:10:21 +0900

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,29 @@
 class BooksController < ApplicationController
 
+before_action :authenticate_user!
+before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
+  def ensure_correct_user
+    @book = Book.find_by(id:params[:id])
+    if @book.user_id != current_user.id
+      flash[:notice] = "このbookは編集できません"
+      redirect_to books_path
+    end
+  end
+
   def show
   	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new #新規投稿用のインスタンス変数
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
@@ -33,16 +47,16 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
